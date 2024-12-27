dd1a6fdc1ab9ffb4ec622c77a21041f11a32935b
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2024-12-27 12:22:27 +0530

    feat(WishListContext.jsx): create WishListContextProvider component for task 9 to manage wishlist items and store them in local storage

---
+++ src/context/WishListContext.jsx
@@ -0,0 +1,12 @@
+// Create context here for task 9
+function WishListContextProvider(props) {
+    // Create store here for task 9
+  
+    // Set Item to local storage here for task 9
+    return (
+      <div>
+        <h1>WishListContext</h1>
+      </div>
+    );
+  }
+  
