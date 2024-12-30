f1a5eb72ddfc83ae802768cb130f18e320c026d9
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2024-12-30 12:23:27 +0530

    style(Book.jsx): improve layout and styling of Book component for better readability and aesthetics style(BookList.jsx): update layout of BookList component to display books in a grid format for improved presentation

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,20 +1,20 @@
 function Book({ id, title, img, excerpt, author }) {
   return (
-    <section class="max-w-sm bg-gray-200 rounded-lg shadow-lg overflow-hidden">
-      <div class="max-w-sm rounded overflow-hidden shadow-lg">
-        <img class="w-full" src={img} alt="Sunset in the mountains" />
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">{title}</div>
-          <p class="text-gray-700 text-base">{excerpt}</p>
-        </div>
-        <div class=" flex justify-between items-center px-6 pt-4 pb-2">
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            {author}
-          </span>
-          <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-            View Details
-          </button>
-        </div>
+    <section class="max-w-sm bg-gray-200 rounded-lg shadow-lg overflow-hidden flex flex-col h-full">
+      <div class="h-48 overflow-hidden">
+        <img class="w-full object-contain" src={img} alt={title} />
+      </div>
+      <div class="flex-grow px-6 py-4 flex flex-col">
+        <div class="font-bold text-xl mb-2">{title}</div>
+        <p class="text-gray-700 text-base flex-grow">{excerpt}</p>
+      </div>
+      <div class="flex justify-between items-center px-6 pt-4 pb-2">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
+          {author}
+        </span>
+        <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          View Details
+        </button>
       </div>
     </section>
   );
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -4,7 +4,7 @@ import data from "../../database/data.json";
 function BookList() {
   const [books] = createSignal(data.books);
   return (
-    <section>
+    <section class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
       <For each={books()}>
         {(book) => (
           <Book
